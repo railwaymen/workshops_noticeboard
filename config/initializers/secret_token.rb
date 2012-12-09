2fd6ef26018439a9198559a53cb9870d7aff1c73
railwaymen <railwaymen@linchworm.(none)>
2012-12-09 21:58:01 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkshopsNoticeboard::Application.config.secret_token = '915c72c4c4a4b0045eea9f5b9b7864d3a62f8b605f9c5550c09bab70ca47451f571e3d52dc360f67dee310197e50f241842df1b8b251112c50fcee238c685659'
